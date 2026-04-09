e45191eb658269e11b910914a9159e95141188f5
Alex Kh. <sanchez.primal@gmail.com>
2026-04-09 13:55:55 +0200

    refactor regex line length

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -14,8 +14,11 @@ const EMPTY_MOVIE_DATA = {
   imdbId: '',
 };
 
-/* eslint-disable-next-line */
-const validURLPattern = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
+const validURLPattern = new RegExp(
+  '^((([A-Za-z]{3,9}:(?://)?)(?:[-;:&=+$,w]+@)?[A-Za-z0-9.-]+|' +
+    '(?:www.|[-;:&=+$,w]+@)[A-Za-z0-9.-]+)' +
+    '((?:/[+~%/.w-_]*)???(?:[-+=&;%@,.w_]*)#?(?:[,.!/\\w]*))?)$',
+);
 
 export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   const [count, setCount] = useState(0);
